bc7f4b75e51f31698fad982f5bd308cc0864cc0c
koussa <hr.xiao@outlook.com>
2023-03-28 11:24:27 +0800

    merge

--- app/src/main/java/com/xhr/fzp/other/Test.kt
+++ app/src/main/java/com/xhr/fzp/other/Test.kt
@@ -1,13 +1,5 @@
 package com.xhr.fzp.other
 
 fun main() {
-    val type = mapOf(
-        "1" to 1,
-        "2" to 2,
-        "3" to 3,
-        "4" to 4
-    )
-    for (key in type.keys) {
-        println(key)
-    }
+    println("测试")
 }
